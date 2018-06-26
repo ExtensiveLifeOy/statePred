c3fe9edce77150203971b39f14e9b3969b565817
Martinez Remberto <remberto.martinez@elisa.fi>
2018-06-26 15:11:06 +0300

    generic classifier

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using Microsoft.ML; // 0.1.0 at 9.5.2018 :-)
-using Microsoft.ML.Runtime.Api;
+using System.Collections.Generic;
+using Microsoft.ML;
+using Microsoft.ML.Models;
+// 0.1.0 at 9.5.2018 :-)
 using Microsoft.ML.Trainers;
 using Microsoft.ML.Transforms;
 
@@ -8,51 +10,75 @@ namespace statePred
 {
     class Program
     {
-         // STEP 1: Define your data structures
-        // use HeliData       
+        // STEP 1: Define your data structures
+        // use a-incidents
+        //  "number","caller_id","category","priority","incident_state","short_description","assigned_to","sys_updated_on","sys_updated_by","assignment_group","company"
      
         static void Main(string[] args)
         {
-            Console.WriteLine("ML for Heli state!");
+            Console.WriteLine("ML for Elisa!");
+            const string modelpath = @".\Data\Model.zip";
 
             // STEP 2: Create a pipeline and load your data
             var pipeline = new LearningPipeline();
 
             // If working in Visual Studio, make sure the 'Copy to Output Directory' 
             // property of dataPath is set to 'Copy always'
-            string dataPath = "feedbacks-19.txt";
-            //pipeline.Add(new TextLoader<IrisData>(dataPath, separator: ","));
-            pipeline.Add(new TextLoader<HeliData>(dataPath, separator: ","));
+            string dataPath = "data/incidents.txt";
+            pipeline.Add(new TextLoader<IncidentData>(dataPath, separator: ","));
 
             // STEP 3: Transform your data
             // Assign numeric values to text in the "Label" column, because only
             // numbers can be processed during model training
-            pipeline.Add(new Dictionarizer("Label"));
+            pipeline.Add(new Dictionarizer("Label"));            
 
-            // Puts all features into a vector            
-            pipeline.Add(new ColumnConcatenator("Features", "Adherence"));
+            // Puts all features into a vector  (ColumnConcatenator is ok for numbers)
+            pipeline.Add(new TextFeaturizer("Features", "ShortDescription"));
+
 
             // STEP 4: Add learner
-            // Add a learning algorithm to the pipeline. 
+            // Add a learning algorithm to the pipeline.  StochasticDualCoordinateAscentClassifier, , LogisticRegressor
             // This is a classification scenario (Question: What type of state is this?)
-            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
-            //pipeline.Add(new NaiveBayesClassifier());
-            //pipeline.Add(new LogisticRegressor());
+            pipeline.Add(new NaiveBayesClassifier()); //FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 });
 
-             // Convert the Label back into original text (after converting to number in step 3)
+            
+            // Convert the Label back into original text (after converting to number in step 3)
             pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
 
             // STEP 5: Train your model based on the data set
-            var model = pipeline.Train<HeliData, HeliPrediction>();
+            var model = pipeline.Train<IncidentData, IncidentCategoryPrediction>();
+
+            model.WriteAsync(modelpath);
 
             // STEP 6: Use your model to make a prediction
-            // You can change these numbers to test different predictions            
-            var inputData = new HeliData()
+            // You can change these numbers to test different predictions
+            Console.WriteLine();
+            Console.WriteLine("PredictionModel evaluation");
+            Console.WriteLine("---------------------------------------------------------------------");
+
+            IEnumerable<IncidentData> incidents = new[]
             {
-                Adherence = 0.3f                
+                new IncidentData()
+                {
+                    ShortDescription = "Intraan ei pääsee",
+                    Label = ""
+                } 
             };
-             var prediction = model.Predict(inputData);
-            Console.WriteLine($"Predicted state type for adh {inputData.Adherence} is: {prediction.PredictedLabels}");
+
+            
+            foreach (var item in incidents)
+            {
+                Console.WriteLine($"Incident \"{item.ShortDescription}\" | Category predicted: {model.Predict(item).PredictedLabels}");                
+            }
+            
+
+            //STEP 7 Evaluate your model
+            var evaluator = new ClassificationEvaluator();
+            var metrics = evaluator.Evaluate(model, new TextLoader<IncidentData>(dataPath, separator: ","));
+            
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.AccuracyMacro:P2}");
+            Console.WriteLine("-----------------------------------------------------------------------");
         }
     }
 }
